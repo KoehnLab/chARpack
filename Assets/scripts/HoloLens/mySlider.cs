3ae2add4d8abf35d81cd48a84e87452939f1ee75
Ayla Flach <st178535@stud.uni-stuttgart.de>
2023-08-18 11:46:42 +0200

    First attempt at implementing mouse interaction, does not work yet

--- Assets/scripts/HoloLens/mySlider.cs
+++ Assets/scripts/HoloLens/mySlider.cs
@@ -588,6 +588,38 @@ namespace Microsoft.MixedReality.Toolkit.UI
         }
         #endregion
 
+        #region Mouse Interaction
+        // DOES NOT WORK YET!
+
+        private Vector3 startingPosition = Vector3.zero;
+
+        public void OnMouseDown()
+        {
+            startingPosition = GlobalCtrl.Singleton.currentCamera.ScreenToWorldPoint(
+                new Vector3(UnityEngine.Input.mousePosition.x, UnityEngine.Input.mousePosition.y, transform.position.z));
+        }
+
+        public void OnMouseDrag()
+        {
+
+            Vector3 newPosition = new Vector3(UnityEngine.Input.mousePosition.x, UnityEngine.Input.mousePosition.y, transform.position.z);
+            var delta = newPosition - startingPosition;
+            var mouseDelta = Vector3.Dot(SliderTrackDirection.normalized, delta);
+
+            if (useSliderStepDivisions)
+            {
+                var stepVal = (mouseDelta / SliderTrackDirection.magnitude > 0) ? sliderStepVal : (sliderStepVal * -1);
+                var stepMag = Mathf.Floor(Mathf.Abs(mouseDelta / SliderTrackDirection.magnitude) / sliderStepVal);
+                SliderValue = Mathf.Clamp(StartSliderValue + (stepVal * stepMag), 0, 1);
+            }
+            else
+            {
+                SliderValue = Mathf.Clamp(StartSliderValue + mouseDelta / SliderTrackDirection.magnitude, 0, 1);
+            }
+        }
+
+        #endregion
+
         #region IMixedRealityPointerHandler
 
         public void OnPointerUp(MixedRealityPointerEventData eventData)
