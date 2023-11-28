9d251659387788fdbf83585b9b6fcf29d8caf006
Ayla Flach <st178535@stud.uni-stuttgart.de>
2023-11-28 15:56:49 +0100

    Refined if statement to make mouse interaction on buttons in server scene work

--- Assets/scripts/HoloLens/buttonMouseClick.cs
+++ Assets/scripts/HoloLens/buttonMouseClick.cs
@@ -1,6 +1,7 @@
 using Microsoft.MixedReality.Toolkit.UI;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// This script is used to make MRTK buttons clickable with the left mouse button.
@@ -12,8 +13,10 @@ public class buttonMouseClick : MonoBehaviour
     {
         if (EventSystem.current.IsPointerOverGameObject()) { return; }
 
+        Scene activeScene = SceneManager.GetActiveScene();
+
         // Avoid double pressing with mouse and finger click; special case for canvas buttons that don't have buttonConfigHelper and OnClick
-        if (GameObject.FindObjectOfType(typeof(Microsoft.MixedReality.Toolkit.Input.RiggedHandVisualizer)) != null || gameObject.GetComponent<CanvasRenderer>())
+        if (GameObject.FindObjectOfType(typeof(Microsoft.MixedReality.Toolkit.Input.RiggedHandVisualizer)) != null || gameObject.GetComponent<CanvasRenderer>() || activeScene.name=="ServerScene")
         {
             GetComponent<PressableButtonHoloLens2>().ButtonPressed.Invoke();
         }
