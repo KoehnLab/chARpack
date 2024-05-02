c11ae0d9ed782b55702bdcd28a7fc812ff0b2daf
rauts <tobias.rau@visus.uni-stuttgart.de>
2024-05-02 17:01:04 +0200

    added svg writing to received fomulas

--- Assets/scripts/network/NetworkManagerServer.cs
+++ Assets/scripts/network/NetworkManagerServer.cs
@@ -1256,6 +1256,17 @@ public class NetworkManagerServer : MonoBehaviour
             Debug.LogError("[structureReceiveComplete] Could not find StructureFormulaManager");
             return;
         }
+
+        //write svg to file
+        var file_path = Path.Combine(Application.streamingAssetsPath, $"{svg_content.Length}.svg") ;
+        if (File.Exists(file_path))
+        {
+            Debug.Log(file_path + " already exists.");
+            return;
+        }
+        var sr = File.CreateText(file_path);
+        sr.Write(svg_content);
+        sr.Close();
     }
 
 #endregion
