d7fc1421942b5be09b45dc21b90690fb85662252
rauts <tobias.rau@visus.uni-stuttgart.de>
2024-06-07 18:55:56 +0200

    added android system keyboard to custom input fields

--- Assets/scripts/client_ui/custom_elements/myInputField.cs
+++ Assets/scripts/client_ui/custom_elements/myInputField.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Microsoft.MixedReality.Toolkit.Experimental.UI;
+using UnityEngine.UI;
+using System;
 
 public class myInputField : MRTKTMPInputField
 {
+    TouchScreenKeyboard keyboard;
+
+#if WINDOWS_UWP
     public override void OnDeselect(BaseEventData eventData)
     {
         return;
         //base.OnDeselect(eventData);
     }
+#endif
+
+#if !WINDOWS_UWP
+    public override void OnSelect(BaseEventData eventData)
+    {
+        base.OnSelect(eventData);
+        SendOnFocus();
+
+        ActivateInputField();
+
+        keyboard = TouchScreenKeyboard.Open(text);
+    }
+#endif
 
 }
