d6fbc943dc91c254fd1f753298e3eca52908a8c6
Ayla Flach <st178535@stud.uni-stuttgart.de>
2024-11-13 15:31:56 +0100

    Added FPS counter to login and main scene, toggleable in debug window

--- Assets/scripts/server_ui/showFPS.cs
+++ Assets/scripts/server_ui/showFPS.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace chARpack
 {
     public class showFPS : MonoBehaviour
     {
         public float deltaTime;
+
         void Update()
         {
             deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
             float fps = 1.0f / deltaTime;
-            GetComponent<TextMeshProUGUI>().text = $"{Mathf.Ceil(fps)} fps";
+            if (SceneManager.GetActiveScene().name.Equals("ServerScene"))
+            {
+                GetComponent<TextMeshProUGUI>().text = $"{Mathf.Ceil(fps)} fps";
+            }
+            else
+            {
+                GetComponent<TextMeshPro>().text = $"{Mathf.Ceil(fps)} fps";
+            }
         }
     }
 }
